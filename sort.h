5ea3719ec430ecce9f33fad3f0990ca2f2969f55
BLACKMouha <mohamedsenghor05gmail.com>
2022-10-08 17:20:37 +0000

    Bubble Sort function prototype

---
+++ sort.h
@@ -0,0 +1,16 @@
+#ifndef SORT_H
+#define SORT_H
+
+/** ====== C header files ====== */
+#include <stdio.h>
+#include <stdlib.h>
+
+/** ====== Structures ====== */
+
+
+/** ====== Prototypes ====== */
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+
+#endif /** SORT_H */
