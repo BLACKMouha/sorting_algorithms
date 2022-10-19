46b10cd5c2b0720d61a2794dce06f1e2d7c7a49f
BLACKMouha <mohamedsenghor05gmail.com>
2022-10-19 15:48:42 +0000

    Optimizing Bubble Sort Algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,26 +10,22 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int tmp, swapped;
+	int tmp;
 
-	if (!array || size < 2)
+	if (!array || size < 2 || !size)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
-		swapped = 0;
-		for (j = 1; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j - 1] > array[j])
+			if (array[j] > array[j + 1])
 			{
-				tmp = array[j - 1];
-				array[j - 1] = array[j];
-				array[j] = tmp;
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				print_array(array, size);
-				swapped = 1;
 			}
 		}
-		if (!swapped)
-			break;
 	}
 }
